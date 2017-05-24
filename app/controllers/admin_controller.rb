a1f5370113519160b232958b90cc0a65239e4826
CHIKURA Shinsaku <shinsaku@chikura.me>
2017-05-25 06:16:20 +0900

    Add admin portal page.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,6 @@
+class AdminController < ApplicationController
+  before_action :verify_admin
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
     mount Que::Web, at: 'que'
   end
 
-  resources :cron_jobs
   get 'health-check' => 'health_check#index'
   post 'tick' => 'clock#tick'
 
-  resources :feed_articles
-  resources :feeds
+  resources :feeds do
+    resources :feed_articles
+  end
   resources :identities
+
   root to: 'root#index'
   get '/home', to: 'root#home', as: :home
 
@@ -18,4 +19,6 @@ Rails.application.routes.draw do
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  get '/admin', to: 'admin#index', as: :admin
+  resources :cron_jobs
 end
