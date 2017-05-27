6f209dc93c1ee5a7aaab6ec5de1a88956739fd76
CHIKURA Shinsaku <shinsaku@chikura.me>
2017-05-27 19:46:57 +0900

    Allow to follow bot after creation

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -70,6 +70,6 @@ class FeedsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def feed_params
-    params.require(:feed).permit(:slug, :url, :avatar, :remote_avatar_url, :verified)
+    params.require(:feed).permit(:slug, :url, :avatar, :remote_avatar_url, :verified, :follow_bot_form)
   end
 end
